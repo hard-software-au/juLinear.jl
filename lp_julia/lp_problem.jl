ab3b8e2bee18820bc6ac809424a40db4e1ad9fc7
Rory Yarr <roryyarr314@gmail.com>
2024-08-05 14:41:26 +1000

    Create lp_problem.jl and update lp_presolve.jl.

---
+++ lp_julia/lp_problem.jl
@@ -0,0 +1,18 @@
+module lpProblem
+
+# Define a struct to represent a Linear Programming problem
+struct LPProblem
+    is_minimize::Bool  # True if the objective is to minimize
+    c::Vector{Float64}  # Objective function coefficients
+    A::Matrix{Float64}  # Constraint matrix
+    b::Vector{Float64}  # Right-hand side of constraints
+    l::Vector{Float64}  # Variable lower bounds
+    u::Vector{Float64}  # Variable upper bounds
+    vars::Vector{String}  # Variable names
+    constraint_types::Vector{Char}  # Constraint types
+end
+
+# Export the LPProblem struct
+export LPProblem
+
+end # module
