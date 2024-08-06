b0ca9919f1a3862abd158cc9cffced43df0ff200
Harley Mackenzie <hjm@hardsoftware.com>
2024-08-06 11:40:49 +1000

    Added utils and constants modules

---
+++ lp_julia/lp_utils.jl
@@ -0,0 +1,17 @@
+module lp_utils
+
+export is_running_in_notebook
+
+function is_running_in_notebook()
+    # Check if running in VS Code's Jupyter notebook environment
+    if (haskey(ENV, "VSCODE_PID") || haskey(ENV, "VSCODE_CWD"))
+        return true
+    # Check if running in a general Jupyter environment (including VS Code)
+    elseif isdefined(Main, :IJulia) && Main.IJulia.inited
+        return true
+    else
+        return false
+    end
+end
+
+end # module
