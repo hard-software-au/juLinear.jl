39876513cfca59816923d7fee08f1bc613c6e3ba
Rory Yarr <roryyarr314@gmail.com>
2024-10-07 14:33:42 +1100

    Add RunAllTests file

---
+++ check/test/RunAllTests.jl
@@ -0,0 +1,10 @@
+# Modules are loaded here.
+push!(LOAD_PATH, abspath(@__DIR__))
+using TestHelpers
+using TestFramework
+
+# All unit tests should be placed in this array.
+tests = ["ReadLP", "ReadMPS", "StandardFormConverter"]
+
+# The test are run here.
+run_tests(tests)
